80d5f9b8ce2efc18f57efc1da0eab8be4d7d8626
NNLoat <83104226+NNLoat@users.noreply.github.com>
2022-12-19 22:40:26 +0700

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,21 @@
+def func_x(num):
+    if num == 1:
+        return 1
+    elif num == 2:
+        return 2
+    elif num == 3:
+        return 3
+    elif num == 4:
+        return 4
+    elif num == 5:
+        return 5
+
+def func_x(is_chubby):
+    if is_chubby is True:
+        return True
+    elif is_chubby is False:
+        return False
+
+def func_x(array):
+    for i in range(len(array)):
+        print(array[i])
