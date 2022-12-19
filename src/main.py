839ad12306b2c01601daf760e5183b8675d56938
NNLoat <83104226+NNLoat@users.noreply.github.com>
2022-12-19 22:46:05 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,21 +1,10 @@
-def func_x(num):
-    if num == 1:
-        return 1
-    elif num == 2:
-        return 2
-    elif num == 3:
-        return 3
-    elif num == 4:
-        return 4
-    elif num == 5:
-        return 5
-
-def func_x(is_chubby):
-    if is_chubby is True:
-        return True
-    elif is_chubby is False:
-        return False
-
-def func_x(array):
-    for i in range(len(array)):
-        print(array[i])
+def main():
+    print "hello" #intentionally wrong
+    L1 = [1,2,3]
+    print (L1[3])
+    
+    D1={'1':"aa", '2':"bb", '3':"cc"}
+    print(D1['4'])
+    
+    
+    print('2'+2)
